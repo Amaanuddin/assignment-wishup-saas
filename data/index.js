faabe2a4be9df3739daa55356a2fb9191defdadf
Amaanuddin <amaanuddinparvez@gmail.com>
2021-12-11 09:12:09 +0530

    added data file

---
+++ data/index.js
@@ -0,0 +1,17 @@
+module.exports.data = {
+  users: [
+    {
+      user_id: 1,
+      user_name: "amaan",
+      created_at: "2021-12-11 09:07:20",
+    },
+  ],
+  subscriptions: [
+    {
+      plan_id: 1,
+      plan_name: "FREE",
+      validity: "-1",
+      price: 0,
+    },
+  ],
+};
